88093f99c474aee948d1b5085cae5911b9b2d911
mgrochalski <michal.grochalski@gmail.com>
2020-09-19 10:26:32 +0200

    add game grid

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    let square = Array.from(document.querySelectorAll('.grid div'));
+    const score = document.querySelector('#score');
+    const startBtn = document.querySelector('#start-button');
+    const test = document.querySelectorAll('.grid div');
+    const width = 10;
 
 });
